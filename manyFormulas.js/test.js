69433f72075c079d371b0fbb47634d1e3d1cc1ba
YoshinoAkira <akirayo.badpingpong@gmail.com>
2018-12-12 03:46:35 +0900

    solution 2

--- manyFormulas.js/test.js
+++ manyFormulas.js/test.js
@@ -2,3 +2,8 @@ const str = "aiueo";
 Array.prototype.forEach.call(str, (item, index) => {
   console.log(`${index}: ${item}`);
 })
+
+
+const sum = 10;
+const str2 = "";
+console.log(sum + Number(str2));
