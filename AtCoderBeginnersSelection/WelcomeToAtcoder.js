b4d694fc4194301eff9feb95dc06c1a157f330a4
badpingpong <akirayo.badpingpong@gmail.com>
2020-08-19 02:52:48 +0900

    tsc for test

---
+++ AtCoderBeginnersSelection/WelcomeToAtcoder.js
@@ -0,0 +1,2 @@
+"use strict";
+//# sourceMappingURL=WelcomeToAtcoder.js.map
---
+++ src/index.js
@@ -0,0 +1,6 @@
+"use strict";
+function hello(name) {
+    return `hello ${name}`;
+}
+console.log(hello('world'));
+//# sourceMappingURL=index.js.map
